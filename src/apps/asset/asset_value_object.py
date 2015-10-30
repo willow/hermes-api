863be357ea92b10b81c99061b7f5b3368ea6c33d
scott coates <scoarescoare@gmail.com>
2015-10-30 15:40:03 -0500

    adding asset_id for asset val obj

--- src/apps/asset/asset_value_object.py
+++ src/apps/asset/asset_value_object.py
@@ -2,5 +2,5 @@ from collections import namedtuple
 
 AssetValueObject = namedtuple(
   'AssetValueObject',
-  'asset_path asset_content_type asset_original_name'
+  'asset_id asset_path asset_content_type asset_original_name'
 )
--- src/apps/asset/services/asset_service.py
+++ src/apps/asset/services/asset_service.py
@@ -5,10 +5,9 @@ from src.libs.django_utils.storage import storage_utils
 from src.apps.asset.asset_value_object import AssetValueObject
 
 
-def _generate_asset_id_from_filename(filename, _file_utils=None):
+def _generate_asset_id_from_filename(asset_id, filename, _file_utils=None):
   if not _file_utils: _file_utils = file_utils
 
-  asset_id = generate_id()
   asset_id_with_ext = "{0}.{1}".format(asset_id, _file_utils.get_file_extension(filename))
 
   return asset_id_with_ext
@@ -24,11 +23,13 @@ def _save_file_to_storage(file, path, _storage_utils=None):
 
 def persist_asset_from_file(file, path):
   original_name = file.name
-  asset_id = _generate_asset_id_from_filename(original_name)
-  path = "{0}/{1}".format(path, asset_id)
+  asset_id = generate_id()
+
+  asset_id_with_ext = _generate_asset_id_from_filename(asset_id, original_name)
+  path = "{0}/{1}".format(path, asset_id_with_ext)
 
   _save_file_to_storage(file, path)
 
-  ret_val = AssetValueObject(path, file.content_type, original_name)
+  ret_val = AssetValueObject(asset_id, path, file.content_type, original_name)
 
   return ret_val
