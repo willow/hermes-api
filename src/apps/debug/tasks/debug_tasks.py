8705bcf4bd740420c4b3f8ea5f375ed7a7e1642e
scott coates <scoarescoare@gmail.com>
2015-11-18 14:31:18 -0600

    added debug and test error task

---
+++ src/apps/debug/tasks/debug_tasks.py
@@ -0,0 +1,28 @@
+import logging
+
+from django_rq import job
+
+from src.libs.python_utils.logging.logging_utils import log_wrapper
+
+logger = logging.getLogger(__name__)
+
+
+@job
+def test_error_task(name='Hello'):
+  log_message = (
+    "Test error task for parameter: name: %s",
+    name
+  )
+  try:
+    raise Exception('This is an error!')
+  except:
+    logger.exception('this is an error log: %s', name)
+
+  rq_logger = logging.getLogger('rq.worker')
+  try:
+    raise Exception('This is an rq worker error!')
+  except:
+    rq_logger.exception('this is an rq worker error log: %s', name)
+
+  with log_wrapper(logger.info, *log_message):
+    raise Exception('ERRORR!@!@#!@# %s' % name)
